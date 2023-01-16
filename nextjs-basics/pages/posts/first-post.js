95d0528818e610d303e0fae51f07c8964fc2a60c
Stenny <stenny.manao@gmail.com>
2023-01-16 12:58:38 +0300

    images & script

--- nextjs-basics/pages/posts/first-post.js
+++ nextjs-basics/pages/posts/first-post.js
@@ -1,12 +1,27 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Script from 'next/script';
 
 export default function FirstPost() {
   return (
-    <>
-      <h1>Première Post</h1>
-      <h2>
-        <Link href="/">Page d'acccueil</Link>
-      </h2>
+      <>
+        <Head>
+            <title>First Blog</title>
+        </Head>
+        <h1>Première Post</h1>
+        <h2>
+            <Link href="/">Page d'acccueil</Link>
+        </h2>
+          <Image src="/images/profile.jpg" height={144} width={144} alt="my profile" />
+          
+          <Script
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnload"
+            onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+            }
+        />
     </>
   );
 }
