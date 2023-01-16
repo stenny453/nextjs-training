3d3f389c1d273a8674805b921230206d6d9a3a83
Stenny <stenny.manao@gmail.com>
2023-01-16 14:18:29 +0300

    Styles next

--- nextjs-basics/pages/posts/first-post.js
+++ nextjs-basics/pages/posts/first-post.js
@@ -1,27 +1,23 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
-import Script from 'next/script';
+// import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-      <>
+      <Layout>
         <Head>
             <title>First Blog</title>
         </Head>
-        <h1>Première Post</h1>
-        <h2>
-            <Link href="/">Page d'acccueil</Link>
-        </h2>
-          <Image src="/images/profile.jpg" height={144} width={144} alt="my profile" />
-          
-          <Script
+        <h1>Première Post</h1>        
+          {/* <Script
             src="https://connect.facebook.net/en_US/sdk.js"
             strategy="lazyOnload"
             onLoad={() =>
-            console.log(`script loaded correctly, window.FB has been populated`)
+                console.log(`script loaded correctly, window.FB has been populated`)
             }
-        />
-    </>
+            /> */}
+    </Layout>
   );
 }
